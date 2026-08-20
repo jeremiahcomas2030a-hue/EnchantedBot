ad4eec657a6d3ddd32c4f8fca015b00550fd16a2
jeremiahcomas2030a-hue <jeremiahcomas2030a@gmail.com>
2026-08-20 10:13:10 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Join Enchanted SMP!",     // this is what people actually see
+        state: "Join Enchanted SMP",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
